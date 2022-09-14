b7388bda8ef17b94fde236baa2c3f091066e875f
BeingMurshid <murshid717104@gmail.com>
2022-09-14 18:19:54 +0500

    this is my first calculator

---
+++ app.js
@@ -0,0 +1,32 @@
+ let string = ""
+  let buttons = document.querySelectorAll('.button')
+//   console.log(buttons)
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        try{
+        if(e.target.innerHTML == '='){
+            string= eval(string)
+            document.querySelector('input').value = string
+ 
+        }else if(e.target.innerHTML == 'c'){
+            string= ""
+            document.querySelector('input').value = string
+ 
+        }
+        else{
+            console.log(e.target)
+            string =  string + e.target.innerHTML;
+            document.querySelector('input').value = string
+
+        }
+
+}
+catch(err){
+document.getElementById('demo').innerHTML=err.message
+}
+
+
+    })
+}) 
+
+// console.log(document.querySelector('input')  )
